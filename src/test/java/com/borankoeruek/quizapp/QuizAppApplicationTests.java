771e28eab9c432c608be79fd644bbb9f93199c96
Boran Körük <borankrk@gmail.com>
2024-05-25 23:14:05 +0200

    changed top level folder structure, added backend, frontend folder

--- src/test/java/com/borankoeruek/quizapp/QuizAppApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.borankoeruek.quizapp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class QuizAppApplicationTests {
-
-}

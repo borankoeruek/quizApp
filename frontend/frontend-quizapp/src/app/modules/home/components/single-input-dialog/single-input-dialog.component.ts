4397ca0215fe785969274f5dd0eeb5f5f57d3c0e
Boran Körük <borankrk@gmail.com>
2024-05-30 22:10:20 +0200

    Created single-input-dialog.component

--- frontend/frontend-quizapp/src/app/modules/home/components/single-input-dialog/single-input-dialog.component.ts
+++ frontend/frontend-quizapp/src/app/modules/home/components/single-input-dialog/single-input-dialog.component.ts
@@ -1,26 +1,19 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-single-input-dialog',
   templateUrl: './single-input-dialog.component.html',
   styleUrl: './single-input-dialog.component.css',
 })
-export class SingleInputDialogComponent implements OnInit {
-  @Input()
-  public title: string;
+export class SingleInputDialogComponent {
+  constructor(
+    private dialogRef: MatDialogRef<SingleInputDialogComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: { title: string; inputLabel: string },
+  ) {}
 
-  @Input()
-  public inputLabel: string;
-
-  @Output()
-  public outputInput: EventEmitter<string> = new EventEmitter<string>();
-
-  constructor(private dialog: MatDialog) {}
-
-  public closeDialog(): void {
-    this.dialog.closeAll();
+  public onClose(inputValue?: string): void {
+    this.dialogRef.close(inputValue);
   }
-
-  public ngOnInit(): void {}
 }
